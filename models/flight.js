cee31bea8a36c88587f495d5863a121cad9f7c32
Hosea Eyob <hosea.eyob@macbook-pro.lan>
2020-08-31 21:40:59 -0500

    create flight model

--- models/flight.js
+++ models/flight.js
@@ -2,14 +2,34 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+// const flightSchema = new Schema({
+//     airline: String,
+//     airport: String,
+//     flightNo: Number,
+//     departs: Date
+// });
+
 const flightSchema = new Schema({
-    airline: String,
-    airport: String,
-    flightNo: Number,
-    departs: Date
-});
-
-
+    airline: {
+        type: String,
+        enum: ['American', 'Delta', 'Southwest', 'United'],
+    },
+    airport: {
+        type: String,
+        enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN',
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+    },
+    departs: {
+        type: Date,
+        enum: [],
+        default: () => Date.now() + 1,
+    },
+})
 
 
 
